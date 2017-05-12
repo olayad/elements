e888095ee0f3d5a3e2f0509b81b4855a49e06dc0
Gregory Sanders <gsanders87@gmail.com>
2017-05-12 16:23:42 -0400

    f'Re-add support for bitcoin headers/proofs in sidechain validation'

--- src/primitives/block.h
+++ src/primitives/block.h
@@ -27,7 +27,7 @@ public:
     ADD_SERIALIZE_METHODS;
 
     template <typename Stream, typename Operation>
-    inline void SerializationOp(Stream& s, Operation ser_action, int nType, int nVersion)
+    inline void SerializationOp(Stream& s, Operation ser_action)
     {
         READWRITE(challenge);
         READWRITE(solution);
