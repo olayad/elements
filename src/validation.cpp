8a10eeb8dc7323e3ddc161e3f3dfdb313d8c7ed8
Gregory Sanders <gsanders87@gmail.com>
2017-05-30 11:13:22 -0400

    f'Add witness commitment when there are only txout witnesses'

--- src/validation.cpp
+++ src/validation.cpp
@@ -3312,12 +3312,6 @@ std::vector<unsigned char> GenerateCoinbaseCommitment(CBlock& block, const CBloc
 {
     std::vector<unsigned char> commitment;
     int commitpos = GetWitnessCommitmentIndex(block);
-        for (size_t o = 0; o < block.vtx[t].vout.size(); o++) {
-            if (!CTxOutWitnessSerializer(REF(block.vtx[t].vout[o])).IsNull()) {
-                fHaveWitness = true;
-                break;
-            }
-        }
     std::vector<unsigned char> ret(32, 0x00);
     if (consensusParams.vDeployments[Consensus::DEPLOYMENT_SEGWIT].nTimeout != 0) {
         if (commitpos == -1) {
@@ -3439,8 +3433,8 @@ bool ContextualCheckBlock(const CBlock& block, CValidationState& state, const Co
             if (block.vtx[i]->HasWitness()) {
                 return state.DoS(100, false, REJECT_INVALID, "unexpected-witness", true, strprintf("%s : unexpected witness data found", __func__));
             }
-            for (size_t o = 0; o < block.vtx[i].vout.size(); o++) {
-                if (!CTxOutWitnessSerializer(REF(block.vtx[i].vout[o])).IsNull()) {
+            for (size_t o = 0; o < block.vtx[i]->vout.size(); o++) {
+                if (!CTxOutWitnessSerializer(REF(block.vtx[i]->vout[o])).IsNull()) {
                     return state.DoS(100, false, REJECT_INVALID, "unexpected-witness", true, strprintf("%s : unexpected output witness data found", __func__));
                 }
             }
