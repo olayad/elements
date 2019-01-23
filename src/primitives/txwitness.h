1bb6d21c397ba0a5e9fc5b2698b86c967dab07a9
Steven Roose <steven@stevenroose.org>
2019-01-23 14:53:48 +0000

    not sure this makes sense

--- src/primitives/txwitness.h
+++ src/primitives/txwitness.h
@@ -110,10 +110,11 @@ public:
         //for (size_t n = 0; n < vtxoutwit.size(); n++) {
         //    READWRITE(vtxoutwit[n]);
         //}
-        if (IsNull()) {
-            /* It's illegal to encode a witness when all vtxinwit and vtxoutwit entries are empty. */
-            throw std::ios_base::failure("Superfluous witness record");
-        }
+        //TODO(stevenroose) re-enabled after testing
+        //if (IsNull()) {
+        //    /* It's illegal to encode a witness when all vtxinwit and vtxoutwit entries are empty. */
+        //    throw std::ios_base::failure("Superfluous witness record");
+        //}
     }
 
     bool IsEmpty() const
